784ad76e7220b3adcd831eb3a15ef8f6aac67a2c
prashant <prashant09hit@gmail.com>
2024-04-03 21:17:08 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// database
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect()
+        
+    } catch (err) {
+        console.log(err)
+        
+    }
+}
+
+app.listen(5000, ()=>{
+    console.log("App is running on port 5000")
+})
