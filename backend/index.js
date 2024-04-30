8acd8c02da05968d494110fb24c952e31a079f50
Shubham Yadav <shubhayadav2503@gmail.com>
2024-04-30 12:41:45 +0530

    Shubham Yadav (backend integration wih UI)

--- backend/index.js
+++ backend/index.js
@@ -31,13 +31,31 @@ const connectDB = async()=>{
 // middlewares
 dotenv.config()
 app.use(express.json())
+app.use("/images",express.static(path.join(__dirname,"/images")))
+
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
 app.use("/api/comments",commentRoute)
 
+// image upload
 
+const storage=multer.diskStorage({
+    destination:(req,file,fn)=>{
+        fn(null,"images")
+    },
+    filename:(req,file,fn)=>{
+        fn(null,req.body.img)
+        
+    }
+})
+
+const upload=multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("Image has been uploaded successfully!")
+})
 
 app.listen(process.env.PORT, ()=>{
     connectDB()
--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -1,39 +1,89 @@
+import { useNavigate, useParams } from "react-router-dom"
 import Comments from "../components/Comments"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import { BiEdit } from 'react-icons/bi'
 import { MdDelete } from 'react-icons/md'
+import { useContext, useEffect, useState } from "react"
+import axios from "axios"
+import { URL,IF } from "../url"
+import { UserContext } from "../context/UserContext"
+import Loader from "../components/Loader"
 
 const PostDetails = () => {
+
+      const postId=useParams().id
+    //   console.log(postId);
+      const[post,setPost]=useState({})
+      const{user}=useContext(UserContext);
+      const [loader,setLoader]=useState(false)
+      const navigate=useNavigate()
+      const fetchPost=async()=>{
+        setLoader(true)
+        try {
+            const res= await axios.get(URL+"/api/posts/"+postId)
+
+            setPost(res.data);
+            setLoader(false)
+            // console.log(res.data);
+        } catch (err) {
+            console.log(err);
+            setLoader(true)
+        }
+      }
+
+      const handleDeletePost=async ()=>{
+
+        try{
+          const res=await axios.delete(URL+"/api/posts/"+postId,{withCredentials:true})
+          console.log(res.data)
+          navigate("/")
+    
+        }
+        catch(err){
+          console.log(err)
+        }
+    
+      }
+
+      useEffect(()=>{
+        fetchPost()
+        
+      },[postId])
+
     return (
         <div>
             <Navbar />
-            <div className="px-8 md:px-[200px] mt-8">
+            {loader?<div className="h-[80vh] flex justify-center items-center w-full"><Loader/></div>:<div className="px-8 md:px-[200px] mt-8">
                 <div className="flex justify-between items-center">
-                    <h1 className="text-2xl font-bold text-black md:text-3xl"> 10 uses of artificial intelligence</h1>
-                    <div className="flex items-center justify-center space-x-2">
-                        <p><BiEdit /></p>
-                        <p><MdDelete /></p>
-
-                    </div>
+                    <h1 className="text-2xl font-bold text-black md:text-3xl">{post.title}</h1>
+                    {user?._id===post?.userId && <div className="flex items-center justify-center space-x-2">
+            <p className="cursor-pointer"  ><BiEdit/></p>
+            <p className="cursor-pointer" onClick={handleDeletePost}><MdDelete/></p>
+         </div>}
                 </div>
                 <div className="flex items-center justify-between mt-2 md:mt-4">
-                    <p>@prashant</p>
+                    <p>@{post.username}</p>
                     <div className="flex space-x-2">
-                        <p>29/03/2024</p>
-                        <p>16:45</p>
+                    <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
 
                     </div>
 
 
                 </div>
-                <img src="https://reactjs.org/logo-og.png" className="w-full mx-auto mt-8" alt="" />
-                <p className="mx-auto mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quos accusantium expedita aliquid laborum odio sit maxime veniam. Voluptatum, animi ea velit, dolorum excepturi facilis dolore odio eaque recusandae possimus enim repudiandae molestiae magnam adipisci? Voluptatem eveniet inventore blanditiis fuga fugiat iusto cupiditate modi quae, tempore voluptate, molestiae, architecto corrupti?</p>
+                <img src={IF+post.photo} className="w-full mx-auto mt-8" alt="" />
+                <p className="mx-auto mt-8">{post.desc}</p>
                 <div className="flex items-center mt-8 space-x-4 font-semibold">
                     <p>Categories:</p>
                     <div className="flex justify-center items-center space-x-2">
-                        <div className="bg-gray-300 rounded-full border-solid border-2 border-gray-500 px-3 py-1">Tech</div>
-                        <div className="bg-gray-300 rounded-full border-solid border-2 border-gray-500 px-3 py-1">AI</div>
+                    {post.categories?.map((c,i)=>(
+            <>
+            <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">{c}</div>
+            </>
+            
+          ))}
+
 
                     </div>
 
@@ -50,7 +100,7 @@ const PostDetails = () => {
                     <input type="text" placeholder="Write a comment" className="md:w-[80%] outline-none py-2 px-4 mt-4 md:mt-0" />
                     <button className="bg-black text-sm text-white px-2 py-2 md:w-[20%] mt-4 md:mt-0 rounded-lg">Add Comment</button>
                 </div>
-            </div>
+            </div>}
             <Footer />
         </div>
     )
